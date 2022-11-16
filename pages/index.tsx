a57790d221e71b1f93755f4be281372202b800ae
AsP3X <zombymedia@gmail.com>
2022-11-16 15:40:51 +0100

    TASK: added layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
+import Head from "next/head";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div>
+      <h1 className="text-2xl">Home</h1>
+      <p className="text-gray-500">Welcome to Next.js!</p>
+
+      <Link href="/">Home</Link>
+    </div>
+  )
 }
